716210a96288dc214f335c4933af0080e7c34509
FelixW <ge73kib@mytum.de>
2023-04-29 17:23:56 +0200

    added html to pdf

---
+++ server/pdfcreator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+
+import pdfkit
+
+def create_pdf():
+    #Define path to wkhtmltopdf.exe
+    path_to_wkhtmltopdf = r'/usr/bin/wkhtmltopdf'
+
+    #Define path to HTML file
+    path_to_file = 'templates/sample.html'
+
+    #Point pdfkit configuration to wkhtmltopdf.exe
+    config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+
+    #Convert HTML file to PDF
+    pdfkit.from_file(path_to_file, output_path='sample.pdf', configuration=config)
