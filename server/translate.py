20ad19d7675d7c9413599025710119a8ff060fe0
FelixW <ge73kib@mytum.de>
2023-04-29 12:58:09 +0200

    added function for translate call to azure

---
+++ server/translate.py
@@ -0,0 +1,38 @@
+import requests, os, uuid
+from dotenv import load_dotenv
+
+
+def translate(original_text, target_language):
+    load_dotenv()
+
+    # Load the values from .env
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
+
+    # Indicate that we want to translate and the API version (3.0) and the target language
+    path = '/translate?api-version=3.0'
+    # Add the target language parameter
+    target_language_parameter = '&to=' + target_language
+    # Create the full URL
+    constructed_url = endpoint + path + target_language_parameter
+
+    # Set up the header information, which includes our subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # Create the body of the request with the text to be translated
+    body = [{ 'text': original_text }]
+
+    # Make the call using post
+    translator_request = requests.post(constructed_url, headers=headers, json=body)
+    # Retrieve the JSON response
+    translator_response = translator_request.json()
+    # Retrieve the translation
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    return translated_text
