d678b424601161daadb854861d91f9ada44767cc
plutokekz <Lukas.Mahr@campus.lmu.de>
2023-04-30 09:40:29 +0200

    ösrgmrö

--- server/pdfcreator.py
+++ server/pdfcreator.py
@@ -7,14 +7,12 @@ from fastapi import HTTPException
 
 def create_pdf(report: str):
 	# Define path to wkhtmltopdf.exe
-	#path_to_wkhtmltopdf = r'/usr/bin/wkhtmltopdf'
+	# path_to_wkhtmltopdf = r'/usr/bin/wkhtmltopdf'
 
 	# Point pdfkit configuration to wkhtmltopdf.exe
-	config = pdfkit.configuration(encoding="utf-8")
-
 	# Convert HTML file to PDF
 	name = f"{uuid.uuid4().hex}.pdf"
-	if pdfkit.from_string(report, output_path=name, configuration=config):#, configuration=config):
+	if pdfkit.from_string(report, output_path=name):  # , configuration=config):
 		return name
 	print("cant generate pdf ??? ")
 	raise HTTPException(status_code=400, detail="Cant generate pdf??")
