421b4de904646726e7dcd35a919aea1a585961fe
plutokekz <Lukas.Mahr@campus.lmu.de>
2023-04-30 08:54:07 +0200

    pdfs sind scheiße

--- server/pdfcreator.py
+++ server/pdfcreator.py
@@ -7,14 +7,14 @@ from fastapi import HTTPException
 
 def create_pdf(report: str):
 	# Define path to wkhtmltopdf.exe
-	path_to_wkhtmltopdf = r'/usr/bin/wkhtmltopdf'
+	#path_to_wkhtmltopdf = r'/usr/bin/wkhtmltopdf'
 
 	# Point pdfkit configuration to wkhtmltopdf.exe
-	config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
+	#config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
 
 	# Convert HTML file to PDF
 	name = f"{uuid.uuid4().hex}.pdf"
-	if pdfkit.from_string(report, output_path=name, configuration=config):
+	if pdfkit.from_string(report, output_path=name):#, configuration=config):
 		return name
 	print("cant generate pdf ??? ")
 	raise HTTPException(status_code=400, detail="Cant generate pdf??")
